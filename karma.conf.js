86dd11c9103ff94a14f38c607bcb515a40cd2004
noamtcohen <noam@ecb.co.il>
2015-05-28 01:56:20 +0300

    fixed karma.conf.js woth new windows launcher

--- karma.conf.js
+++ karma.conf.js
@@ -56,7 +56,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['C:\Dev\Narma\bat-launcher.bat'],
+    browsers: ['C:/Dev/Narma/bat-launcher.bat'],
 
 
     // Continuous Integration mode
