35de4bb1b72a1a514abbb8634f1baede2ef326b1
noamtcohen <noam@ecb.co.il>
2015-05-28 01:53:04 +0300

    Add windows launcher

--- karma.conf.js
+++ karma.conf.js
@@ -56,7 +56,7 @@ module.exports = function(config) {
 
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['/Users/noam/Dev/Narma/nodewebkit-lanucher.sh'],
+    browsers: ['C:\Dev\Narma\bat-launcher.bat'],
 
 
     // Continuous Integration mode
