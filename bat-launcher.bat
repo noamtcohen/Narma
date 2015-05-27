63be3623df946a491fdeb8adfd6a78999ba74b1e
noamtcohen <noam@ecb.co.il>
2015-05-28 02:15:24 +0300

    fix indent

--- bat-launcher.bat
+++ bat-launcher.bat
@@ -1,18 +1,18 @@
-echo { 								>  test/package.json
-echo  "name": "Narma", 				>> test/package.json
-echo    "main": "%1%", 				>> test/package.json
-echo    "node-remote":"<local>",	>> test/package.json
-echo    "window": {					>> test/package.json
-echo      "title": "Narma",			>> test/package.json
-echo      "toolbar": true,			>> test/package.json
-echo      "frame": true,			>> test/package.json
-echo      "width": 400,				>> test/package.json
-echo      "height": 300,			>> test/package.json
-echo      "position": "center"		>> test/package.json
-echo  },							>> test/package.json
-echo    "webkit": {					>> test/package.json
-echo  	"plugin": true				>> test/package.json
-echo    }							>> test/package.json
-echo } 								>> test/package.json
+echo {                            >  test/package.json
+echo  "name": "Narma",            >> test/package.json
+echo    "main": "%1%",            >> test/package.json
+echo    "node-remote":"<local>",  >> test/package.json
+echo    "window": {               >> test/package.json
+echo      "title": "Narma",       >> test/package.json
+echo      "toolbar": true,        >> test/package.json
+echo      "frame": true,          >> test/package.json
+echo      "width": 400,           >> test/package.json
+echo      "height": 300,          >> test/package.json
+echo      "position": "center"    >> test/package.json
+echo  },                          >> test/package.json
+echo    "webkit": {               >> test/package.json
+echo  	"plugin": true            >> test/package.json
+echo    }                         >> test/package.json
+echo }                            >> test/package.json
 
 nodewebkit test/
